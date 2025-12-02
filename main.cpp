55e202a28b8b10b9bb4509c207637970ad2a23cd
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-12-01 20:01:45 -0800

    Setting up lab 37

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// COMSC-210 | Lab 36 | Dat Hoang Vien
+// IDE used: Visual Studio Code
+
+// Libraries
+#include <iostream>
+using namespace std;
+
+// Main function
+int main()
+{
+    char a = 'A';
+    cout << a << endl;
+    cout << (int) a << endl;
+    int b = 66;
+    cout << b << endl;
+    cout << (char) b << endl;
+
+    return 0;
+}
+
+/* 
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
